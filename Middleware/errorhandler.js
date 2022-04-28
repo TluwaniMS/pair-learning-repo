8cdf49dcf77791c9f397d63e01ba9aaac004075a
tluwaniMS <tluwani2020@gmail.com>
2022-04-28 14:44:18 +0200

    Adding Routes

---
+++ Middleware/errorhandler.js
@@ -0,0 +1,31 @@
+const { ErrorMessages } = require("../enumerators/error-messages");
+
+const errorHandler = (handler) => {
+    return async (req, res, next) => {
+      try {
+        await handler(req, res);
+      } catch (error) {
+        next(error);
+      }
+    };
+  };
+
+  const unknownRequests = (req, res, next) => {
+    const error = new Error(ErrorMessages.badRequestErrorMessage);
+    error.status = 404;
+    next(error);
+  };
+
+  const errorResponse = (error, req, res, next) => {
+    if (error.status) {
+      console.log(error);
+      res.status(error.status).send({ message: error.message });
+    } else {
+      console.log(error);
+      res.status(500).send({ message: ErrorMessages.internalServerErrorMessage });
+    }
+  };
+
+  module.exports = { errorHandler, unknownRequests, errorResponse };
+
+
--- Routes/users-route.js
+++ Routes/users-route.js
@@ -1,4 +1,58 @@
 const express = require("express");
 const router = express.Router();
+const { errorHandler } = require("../Middleware/errorhandler");
+const { getAllUsers, updateUser, createUser, getUserById } = require("../database-queries/UserDBQueries");
+const { deleteUserService } = require("../Services/user-services");
 
-module.exports = router
+router.get(
+  "/get-all-users",
+  errorHandler(async (req, res) => {
+    const allUsers = await getAllUsers();
+
+    res.status(200).send({ data: allUsers });
+  })
+);
+
+router.delete(
+  "/delete-user/:id",
+  errorHandler(async (req, res) => {
+    await deleteUserService();
+
+    res.status(200).send({ message: `succesfully deleted` });
+  })
+);
+
+router.put(
+  "/update-user/:id",
+  errorHandler(async (req, res) => {
+    const { id } = req.params;
+    const { name, surname, email } = req.body;
+
+    await updateUser(id, { name: name, surname: surname, email: email });
+
+    res.status(200).send({ message: `succesfully updated` });
+  })
+);
+
+router.post(
+  "/create-user",
+  errorHandler(async (req, res) => {
+    const { name, surname, email } = req.body;
+
+    await createUser({ name: name, surname: surname, email: email });
+
+    res.status(200).send({ message: `sucessfully created` });
+  })
+);
+
+router.get(
+  "/get-user/:id",
+  errorHandler(async (req, res) => {
+    const { id } = req.params;
+    const user = await getUserById(id);
+
+    res.status(200).send(user);
+  })
+);
+
+module.exports = router;
