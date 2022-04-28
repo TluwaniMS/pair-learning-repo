1a9b335c9c2ef5839c823b810639b94bfb48e837
tluwaniMS <tluwani2020@gmail.com>
2022-04-28 11:52:11 +0200

    adding book services

---
+++ Services/book-services.js
@@ -0,0 +1,17 @@
+const { createBook } = require("../database-queries/BooksDBQueries");
+const { linkBookToUser } = require("../database-queries/UserDBQueries");
+
+const createBookService = async ({ title, authorId, description, price }) => {
+  const createdBook = await createBook({
+    title: title,
+    author: authorId,
+    description: description,
+    price: price
+  });
+
+  await linkBookToUser(authorId, createBook._id);
+
+  return `Operation completed successfully.`;
+};
+
+module.exports = { createBookService };
