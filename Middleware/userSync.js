b8fa8090d532aa0a8a95aafd0057f65faa20edbb
tluwaniMS <tluwani2020@gmail.com>
2022-04-29 06:15:36 +0200

    updating-create-user-logic

--- Middleware/userSync.js
+++ Middleware/userSync.js
@@ -7,7 +7,7 @@ const userSyncHandler = async (req, res, next) => {
       const user = await getUserByEmail(tokenInformation.email);
   
       if (!user) {
-        const createdUser = await createUser(tokenInformation);
+        const createdUser = await createUser({name:tokenInformation.name, surname:tokenInformation.surname, email :tokenInformation.email});
         req.user = createdUser;
       } else {
         req.user = user;
